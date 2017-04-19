acd21319ebf72d1933f9ac2c652cce4b0db8b8fb
mwinslow331 <michael.winslow331@gmail.com>
2017-04-19 16:25:53 -0400

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419151455) do
+ActiveRecord::Schema.define(version: 20170419154626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "title",        null: false
+    t.string   "description",  null: false
+    t.string   "release_year"
+    t.string   "platform",     null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,14 +39,6 @@ ActiveRecord::Schema.define(version: 20170419151455) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "games", force: :cascade do |t|
-    t.string   "title",        null: false
-    t.string   "description",  null: false
-    t.string   "release_year"
-    t.string   "platform",     null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
   end
 
 end
