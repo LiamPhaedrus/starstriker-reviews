9776db51a9c94f9cb0369d42442ec8513adee7ba
mwinslow331 <michael.winslow331@gmail.com>
2017-04-20 12:08:06 -0400

    tests now passing for user sign up

--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -10,20 +10,10 @@ feature 'create account', %Q{
     # The button does not appear if you are signed in
     # I must enter valid information to sign up
     # If I do enter valid information, I receive an on screen success message
-    # If I enter bad information, I recieve an on screen error message and am not signed up
+    # If I enter bad information, I recieve an on screen error message and am
+    # not signed up
 
     scenario 'button goes to create account page' do
-      #scenario: a user can successfully sign in with good credentials
-      #steps :go to the root path
-      #click the link to sign in
-      #fill in email with a good Email
-      #fill in password with good password
-      #click link sign in
-
-      #expecations
-      #expect page to have content "welcome"
-      #expect page to have content "sign out"
-
       visit root_path #further changes can be made
       click_link 'Sign up'
       click_link 'Log in'
@@ -31,28 +21,9 @@ feature 'create account', %Q{
       expect(page).to have_content('Log in')
     end
 
-    # scenario 'signed in, log in button does not exist' do
-    #   old_user = User.create(email: 'JoeShmo@gmail.com', password: 'something', password_confirmation: 'something')
-    #   visit root_path
-    #   expect(page).to have_content('Logout')
-    #   expect(page).to_not have_content('Log in')
-    #sad path
-    #a user is not signed in if they give bad credentials
-    #steps :go to the root path
-    #click the link to sign in
-    #click link sign in
-
-    #expectations
-    #expect page to not have content Welcome
-    #expect page to not have content sign out
-    #expect page to have content "you were not signed in"
-    #expect page to have content "sign in"
-    # end
-
-
     scenario 'specifying valid information' do
       visit new_user_registration_path
-      fill_in 'Email', with: 'JoeShmo@gmail.com'
+      fill_in 'Email', with: 'Koala@gmail.com'
       fill_in 'Password', with: 'password'
       fill_in 'Password confirmation', with: 'password'
       click_button 'Sign up'
@@ -61,7 +32,7 @@ feature 'create account', %Q{
 
     scenario 'passwords do not match' do
       visit new_user_registration_path
-      fill_in 'Email', with: 'JoeShmo@gmail.com'
+      fill_in 'Email', with: 'X12@gmail.com'
       fill_in 'Password', with: 'password'
       fill_in 'Password confirmation', with: 'psassword'
       click_button 'Sign up'
@@ -70,7 +41,11 @@ feature 'create account', %Q{
 
 
     scenario 'user name is not unique' do
-      old_user = User.create(email: 'JoeShmo@gmail.com', password: 'something', password_confirmation: 'something')
+      old_user = User.create(
+        email: 'JoeShmo@gmail.com',
+        password: 'something',
+        password_confirmation: 'something'
+        )
       visit new_user_registration_path
       fill_in 'Email', with: 'JoeShmo@gmail.com'
       fill_in 'Password', with: 'password'
@@ -81,12 +56,13 @@ feature 'create account', %Q{
 
     scenario 'valid information is submitted' do
       visit new_user_registration_path
-      fill_in 'Email', with: 'JoeShmo@gmail.com'
+      fill_in 'Email', with: 'Xris@gmail.com'
       fill_in 'Password', with: 'password'
       fill_in 'Password confirmation', with: 'password'
       click_button 'Sign up'
 
       expect(page).to have_content("Welcome! You have signed up successfully.")
-    end
+    end`
+    #expect page to have content "sign out"
   end
 # end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-# 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require "shoulda-matchers"
+
+describe User, type: :model do
+  User.create(
+    email: 'JoeShmo@gmail.com',
+    password: 'something',
+    password_confirmation: 'something'
+    )
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+
+end
