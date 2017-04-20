8776155cd358597a1521eb0a25711727848c650f
mwinslow331 <michael.winslow331@gmail.com>
2017-04-20 10:36:21 -0400

    Stopped requiring sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'user#index'
 
-  root 'games#index'
+  root to: 'games#index'
   resources :games, only: [:index, :show]
 
+  devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
