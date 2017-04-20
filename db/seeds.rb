d1017a9b55b95c1c3a33b89ef337e02a9d5fea14
LiamPhaedrus <liamphaedrus@gmail.com>
2017-04-20 09:56:33 -0400

    Added foundation and a header bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat diam ac orci cursus iaculis. Morbi quis tincidunt quam. Cras tincidunt eros id pretium scelerisque. Nunc nec velit ultricies, hendrerit sem vitae, rutrum risus. Donec non odio non lectus dapibus ultricies quis ac odio. Etiam eu ex vel est maximus bibendum. Integer mi neque, efficitur sed aliquam ac, mattis id neque. Maecenas lobortis sed orci eget ultrices. Quisque venenatis orci in lacinia hendrerit.
