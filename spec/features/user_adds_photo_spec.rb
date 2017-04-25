6e8d019831f5a27dbd0f05ec4404c091e152885c
mwinslow331 <michael.winslow331@gmail.com>
2017-04-25 09:45:06 -0400

    profile page with avatar

--- spec/features/user_adds_photo_spec.rb
+++ spec/features/user_adds_photo_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
   feature 'user adds avatar' do
 
-  scenario 'valid information is submitted' do
+  scenario 'valid image is submitted' do
     visit new_user_registration_path
     fill_in 'Email', with: 'Xris@gmail.com'
     fill_in 'Username', with: 'Xris'
@@ -11,7 +11,7 @@ require 'rails_helper'
     attach_file :profile_photo, "#{Rails.root}/spec/support/images/Starstriker.png"
     click_button 'Sign up'
 
-    visit user_path
+    visit user_path(User.find_by(email: "xris@gmail.com"))
 
     expect(page).to have_css("img[src*='Starstriker.png']")
   end
