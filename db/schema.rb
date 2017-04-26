00d80b4b83fa5e9a90fed35bb227c2a398ddb67d
Philip10023 <philipormorgan@gmail.com>
2017-04-26 13:47:54 -0400

    hiiiii i want the latest and greatest

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170425134751) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +46,10 @@ ActiveRecord::Schema.define(version: 20170425134751) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "username",               default: "",    null: false
     t.boolean  "admin",                  default: false
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username",               default: "", null: false
     t.string   "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
