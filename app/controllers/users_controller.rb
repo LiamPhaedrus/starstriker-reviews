91d8572628ddf0d898d66bf6aa58069e503c70ad
Philip10023 <philipormorgan@gmail.com>
2017-04-26 16:56:53 -0400

    corrected hound errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to users_path, :notice => "User Account Deleted"
+    redirect_to users_path, notice: "User Account Deleted"
   end
 end
