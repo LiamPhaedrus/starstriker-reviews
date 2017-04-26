1febb8e38bd717874f63146cabc0c50de6854866
mwinslow331 <michael.winslow331@gmail.com>
2017-04-26 10:36:01 -0400

    deployed to heroku

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -13,9 +13,9 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
   # def download_url(filename)
   #   url(response_content_disposition: %Q{attachment; filename="#{filename}"})
   # end
