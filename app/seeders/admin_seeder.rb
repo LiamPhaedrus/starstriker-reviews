3b51a9a5e6d8f4ddb45e6562a285f86baeffc199
Philip10023 <philipormorgan@gmail.com>
2017-04-27 16:33:17 -0400

    hound violations part 3

--- app/seeders/admin_seeder.rb
+++ app/seeders/admin_seeder.rb
@@ -7,19 +7,22 @@ class AdminSeeder
       password: 123456
     )
     User.create(
-    username: 'Mike',
-    email: 'michael.winslow331@gmail.com',
-    admin: true,
-    password: 123456)
+      username: 'Mike',
+      email: 'michael.winslow331@gmail.com',
+      admin: true,
+      password: 123456
+    )
     User.create(
-    username: 'Liam',
-    email: 'liamphaedrusgmail.com',
-    admin: true,
-    password: 123456)
+      username: 'Liam',
+      email: 'liamphaedrusgmail.com',
+      admin: true,
+      password: 123456
+    )
     User.create(
-    username: 'Matt',
-    email: 'matthewriebow@gmail.com',
-    admin: true,
-    password: 123456)
+      username: 'Matt',
+      email: 'matthewriebow@gmail.com',
+      admin: true,
+      password: 123456
+    )
   end
 end
