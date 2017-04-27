aeb6b69aace90f5d9d84802c21707692c26deca2
Philip10023 <philipormorgan@gmail.com>
2017-04-27 16:22:33 -0400

    hound violations

--- app/seeders/admin_seeder.rb
+++ app/seeders/admin_seeder.rb
@@ -1,13 +1,17 @@
 class AdminSeeder
 
   def self.seed!
-    User.create (username: 'philip', email: 'philipormorgan@gmail.com',
+    User.create(
+      username: 'philip',
+      email: 'philipormorgan@gmail.com',
+      admin: true,
+      password: 123456
+    )
+    User.create(username: 'Mike', email: 'michael.winslow331@gmail.com',
     admin: true, password: 123456)
-    User.create (username: 'Mike', email: 'michael.winslow331@gmail.com',
+    User.create(username: 'Liam', email: 'liamphaedrusgmail.com',
     admin: true, password: 123456)
-    User.create (username: 'Liam', email: 'liamphaedrusgmail.com',
-    admin: true, password: 123456)
-    User.create (username: 'Matt', email: 'matthewriebow@gmail.com',
+    User.create(username: 'Matt', email: 'matthewriebow@gmail.com',
     admin: true, password: 123456)
   end
 end
